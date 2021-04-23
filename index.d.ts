54a9536552e4d836dfa43f5655688092d9827c45
takayama <takayama@foxmail.com>
2021-04-23 16:30:57 +0900

    优化提升20倍性能

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export function encrypt(data: Buffer, key: Buffer): Buffer;
+export function decrypt(encrypted: Buffer, key: Buffer): Buffer;
--- index.js
+++ index.js
@@ -1,196 +1,132 @@
-var Long = require('./goog.math.Long');
+"use strict";
 
-var op = new Long(0xffffffff)
+const table = [
+    0x9e3779b9,
+    0x3c6ef372,
+    0xdaa66d2b,
+    0x78dde6e4,
+    0x1715609d,
+    0xb54cda56,
+    0x5384540f,
+    0xf1bbcdc8,
+    0x8ff34781,
+    0x2e2ac13a,
+    0xcc623af3,
+    0x6a99b4ac,
+    0x08d12e65,
+    0xa708a81e,
+    0x454021d7,
+    0xe3779b90,
+];
 
-function xor(a, b){
-    var a1 = a.readUInt32BE(0)
-    var a2 = a.readUInt32BE(4)
-
-    var b1 = b.readUInt32BE(0)
-    var b2 = b.readUInt32BE(4)
-
-    var r = new Buffer(8)
-    r.writeInt32BE(( a1 ^ b1) & op,0)
-    r.writeInt32BE(( a2 ^ b2) & op,4)
-    return r
+function _safemod(v, m) {
+    if (v < 0) {
+        v = 0xffffffff + v + 1;
+    }
+    return v % m;
 }
 
-function encipher(v, k){
-    /*
-    TEA encipher encrypt 64 bits value, by 128 bits key,
-    QQ do 16 round TEA.
-    To see:
-    http://www.ftp.cl.cam.ac.uk/ftp/papers/djw-rmn/djw-rmn-tea.html .
-    TEA 加密,  64比特明码, 128比特密钥, qq的TEA算法使用16轮迭代
-    具体参看
-    http://www.ftp.cl.cam.ac.uk/ftp/papers/djw-rmn/djw-rmn-tea.html
-    >>> c = encipher('abcdefgh', 'aaaabbbbccccdddd')
-    >>> b2a_hex(c)
-    'a557272c538d3e96'
-    */
-    var n=16
-    var delta = Long.fromInt(0x9E3779B9)
-
-    var k = [
-        Long.fromInt(k.readUInt32BE(0))
-        , Long.fromInt(k.readUInt32BE(4))
-        , Long.fromInt(k.readUInt32BE(8))
-        , Long.fromInt(k.readUInt32BE(12))
-    ]
-    var y = Long.fromInt(v.readUInt32BE(0))
-    var z = Long.fromInt(v.readUInt32BE(4))
-
-    var s = new Long(0)
-
-    for(var i=0;i<n;i++){
-        s = s.add(delta)
-
-        //y += (op &(z << 4))+ k[0] ^ z+ s ^ (op&(z >> 5)) + k[1]
-        //y &= op
-        y = y.add(
-                op.and(z.shiftLeft(4)).add(k[0])
-                .xor( z.add(s) )
-                .xor( op.and(z.shiftRight(5)).add(k[1]) )
-            )
-            .and(op)
-
-        //z += (op &(y << 4))+ k[2] ^ y+ s ^ (op&(y >> 5)) + k[3]
-        //z &= op
-        z = z.add(
-                op.and(y.shiftLeft(4)).add(k[2])
-                    .xor(y.add(s))
-                    .xor(op.and(y.shiftRight(5)).add(k[3]))
-            )
-            .and(op)
-    }
-
-    var r = new Buffer(8)
-    r.writeUInt32BE(y.toNumber(),0)
-    r.writeUInt32BE(z.toNumber(),4)
-    return r
+function _toUInt32(num) {
+    return num > 0xffffffff ? (num - 4294967296) : num;
 }
 
-
-function safemod(v,m){
-    // 负数取模，先转换为 uint 正数
-    if(v<0){
-        v = 0xffffffff + v + 1
+function _encrypt(x, y, k0, k1, k2, k3) {
+    for (let i = 0; i < 16; ++i) {
+        let aa = (_toUInt32((y << 4 >>> 0) + k0) ^ _toUInt32(y + table[i])) >>> 0 ^ _toUInt32(~~(y / 32) + k1);
+        aa >>>= 0;
+        x = _toUInt32(x + aa);
+        let bb = (_toUInt32((x << 4 >>> 0) + k2) ^ _toUInt32(x + table[i])) >>> 0 ^ _toUInt32(~~(x / 32) + k3);
+        bb >>>= 0;
+        y = _toUInt32(y + bb);
     }
-    return v%m
+    return [x, y];
 }
 
-var END_CHARS = new Buffer([0,0,0,0,0,0,0])
-exports.encrypt = function (v, k){
-    /*
-    Encrypt Message follow QQ's rule.
-    用QQ的规则加密消息
-    参数 v 是被加密的明文, k是密钥
-    >>> en = encrypt('', b2a_hex('b537a06cf3bcb33206237d7149c27bc3'))
-    >>> decrypt(en,  b2a_hex('b537a06cf3bcb33206237d7149c27bc3'))
-    */
-    // END_CHAR = '\0'
-    FILL_N_OR = 0xF8
-
-    var vl = v.length
-    var filln = safemod(8-(vl+2),8) + 2
-
-    var fills = new Buffer(filln)
-    for(var i=0;i<filln;i++){
-        fills.writeUInt8( Math.floor(Math.random()*256), i )
+/**
+ * qqtea encrypt
+ * @param {Buffer} data 
+ * @param {Buffer} key length = 16
+ */
+function encrypt(data, key) {
+    let filln = _safemod(8 - (data.length + 2), 8) + 2;
+    let fills = Buffer.allocUnsafe(filln);
+    const v = Buffer.concat([
+        Buffer.from([(filln - 2) | 0xF8]),
+        fills,
+        data,
+        Buffer.alloc(7)
+    ]);
+    const encrypted = Buffer.allocUnsafe(v.length);
+    const k0 = key.readUInt32BE(0);
+    const k1 = key.readUInt32BE(4);
+    const k2 = key.readUInt32BE(8);
+    const k3 = key.readUInt32BE(12);
+    let r1 = 0, r2 = 0, t1 = 0, t2 = 0;
+    for (let i = 0; i < v.length; i += 8) {
+        const a1 = v.readUInt32BE(i);
+        const a2 = v.readUInt32BE(i + 4);
+        const b1 = a1 ^ r1;
+        const b2 = a2 ^ r2;
+        const [x, y] = _encrypt(b1 >>> 0, b2 >>> 0, k0, k1, k2, k3);
+        r1 = x ^ t1;
+        r2 = y ^ t2;
+        t1 = b1;
+        t2 = b2;
+        encrypted.writeInt32BE(r1, i);
+        encrypted.writeInt32BE(r2, i + 4);
     }
-
-    v = Buffer.concat([
-        new Buffer([(filln -2)|FILL_N_OR])
-        , fills
-        , v
-        , END_CHARS
-    ])
-    var tr = new Buffer([0,0,0,0, 0,0,0,0])
-    var to = new Buffer([0,0,0,0, 0,0,0,0])
-
-    r = new Buffer(0)
-    for(var i=0;i<v.length;i+=8){
-        var o = xor(v.slice(i,i+8), tr)
-        tr = xor( encipher(o, k), to)
-        to = o
-        r = Buffer.concat([r,tr])
-    }
-    return r
+    return encrypted;
 }
 
-
-function decipher(v, k){
-
-    var n = 16
-
-    var y = Long.fromInt(v.readUInt32BE(0))
-    var z = Long.fromInt(v.readUInt32BE(4))
-
-    var a = Long.fromInt(k.readUInt32BE(0))
-    var b = Long.fromInt(k.readUInt32BE(4))
-    var c = Long.fromInt(k.readUInt32BE(8))
-    var d = Long.fromInt(k.readUInt32BE(12))
-
-    var delta = Long.fromInt(0x9E3779B9)
-
-    var s = delta.shiftLeft(4).and(op)
-    for(var i=0;i<n;i++){
-
-        // z -= ((y << 4) + c) ^ (y + s) ^ ((y >> 5) + d)
-        // z &= op
-        z = z.subtract(
-                y.shiftLeft(4).add(c)
-                    .xor( y.add(s) )
-                    .xor( y.shiftRight(5).add(d) )
-            )
-            .and(op)
-
-        //y -= ((z << 4) + a) ^ (z + s) ^ ((z >> 5) + b)
-        //y &= op
-        y = y.subtract(
-                z.shiftLeft(4).add(a)
-                    .xor(z.add(s))
-                    .xor(z.shiftRight(5).add(b))
-            )
-            .and(op)
-
-        s = s.subtract(delta)
+function _decrypt(x, y, k0, k1, k2, k3) {
+    for (let i = 15; i >= 0; --i) {
+        let aa = (_toUInt32((x << 4 >>> 0) + k2) ^ _toUInt32(x + table[i])) >>> 0 ^ _toUInt32(~~(x / 32) + k3);
+        y = y - aa >>> 0;
+        let bb = (_toUInt32((y << 4 >>> 0) + k0) ^ _toUInt32(y + table[i])) >>> 0 ^ _toUInt32(~~(y / 32) + k1);
+        x = x - bb >>> 0;
     }
-    var r = new Buffer(8)
-    r.writeUInt32BE(y.toNumber(),0)
-    r.writeUInt32BE(z.toNumber(),4)
-    return r
+    return [x, y];
 }
 
-
-
-exports.decrypt = function(v, k){
-
-    var l = v.length
-    if(l%8){
-        throw new Error("v 必须是8的倍数")
+/**
+ * qqtea decrypt
+ * @param {Buffer} encrypted 
+ * @param {Buffer} key length = 16
+ */
+function decrypt(encrypted, key) {
+    if (encrypted.length % 8) {
+        throw new Error("length of encrypted data must be a multiple of 8");
     }
-    var prePlain = decipher(v,k)
-
-    var pos = (prePlain[0] & 0x07) +2
-    var r = prePlain
-    var preCrypt = v.slice(0,8)
-
-    for(var i=8;i<l;i+=8){
-
-        var h = xor(v.slice(i,i+8), prePlain)
-        var x = xor(decipher(h,k), preCrypt)
-        prePlain = xor(x, preCrypt)
-        preCrypt = v.slice(i,i+8)
-        r = Buffer.concat([r,x])
-        // r += x
+    const decrypted = Buffer.allocUnsafe(encrypted.length);
+    const k0 = key.readUInt32BE(0);
+    const k1 = key.readUInt32BE(4);
+    const k2 = key.readUInt32BE(8);
+    const k3 = key.readUInt32BE(12);
+    let r1 = 0, r2 = 0, t1 = 0, t2 = 0, x = 0, y = 0;
+    for (let i = 0; i < encrypted.length; i += 8) {
+        const a1 = encrypted.readUInt32BE(i);
+        const a2 = encrypted.readUInt32BE(i + 4);
+        const b1 = a1 ^ x;
+        const b2 = a2 ^ y;
+        [x, y] = _decrypt(b1 >>> 0, b2 >>> 0, k0, k1, k2, k3);
+        r1 = x ^ t1;
+        r2 = y ^ t2;
+        t1 = a1;
+        t2 = a2;
+        decrypted.writeInt32BE(r1, i);
+        decrypted.writeInt32BE(r2, i + 4);
     }
-
-    // 后7位为 00
-    for(var i=1;i<=7;i++){
-        if( r.readUInt8(r.length-i) != 0x00 ){
-            return null
+    for (let i = 1; i <= 7; ++i) {
+        if (decrypted[decrypted.length - i] !== 0x0) {
+            throw new Error("encrypted data is not illegal");
         }
     }
-    return r.slice(pos+1,r.length-7)
+    return decrypted.slice(
+        (decrypted[0] & 0x07) + 3,
+        decrypted.length - 7
+    );
 }
+
+module.exports = {
+    encrypt, decrypt
+};
